e4a3ea9019e2eb049b7ad3960e69df246741b8fe
Jasper Gregory <jaspergregory@gmail.com>
2011-05-24 11:38:42 -0700

    first checkin

---
+++ Classes/ImageScrollView.m
@@ -0,0 +1,190 @@
+
+#import "ImageScrollView.h"
+#import "TilingView.h"
+
+@implementation ImageScrollView
+@synthesize index;
+
+- (id)initWithFrame:(CGRect)frame
+{
+    if ((self = [super initWithFrame:frame])) {
+        self.showsVerticalScrollIndicator = NO;
+        self.showsHorizontalScrollIndicator = NO;
+        self.bouncesZoom = YES;
+        self.decelerationRate = UIScrollViewDecelerationRateFast;
+        self.delegate = self;        
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [imageView release];
+    [super dealloc];
+}
+
+#pragma mark -
+#pragma mark Override layoutSubviews to center content
+
+- (void)layoutSubviews 
+{
+    [super layoutSubviews];
+    
+    // center the image as it becomes smaller than the size of the screen
+    
+    CGSize boundsSize = self.bounds.size;
+    CGRect frameToCenter = imageView.frame;
+    
+    // center horizontally
+    if (frameToCenter.size.width < boundsSize.width)
+        frameToCenter.origin.x = (boundsSize.width - frameToCenter.size.width) / 2;
+    else
+        frameToCenter.origin.x = 0;
+    
+    // center vertically
+    if (frameToCenter.size.height < boundsSize.height)
+        frameToCenter.origin.y = (boundsSize.height - frameToCenter.size.height) / 2;
+    else
+        frameToCenter.origin.y = 0;
+    
+    imageView.frame = frameToCenter;
+    
+    if ([imageView isKindOfClass:[TilingView class]]) {
+        // to handle the interaction between CATiledLayer and high resolution screens, we need to manually set the
+        // tiling view's contentScaleFactor to 1.0. (If we omitted this, it would be 2.0 on high resolution screens,
+        // which would cause the CATiledLayer to ask us for tiles of the wrong scales.)
+        imageView.contentScaleFactor = 1.0;
+    }
+}
+
+#pragma mark -
+#pragma mark UIScrollView delegate methods
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return imageView;
+}
+
+#pragma mark -
+#pragma mark Configure scrollView to display new image (tiled or not)
+
+- (void)displayImage:(UIImage *)image
+{
+    // clear the previous imageView
+    [imageView removeFromSuperview];
+    [imageView release];
+    imageView = nil;
+    
+    // reset our zoomScale to 1.0 before doing any further calculations
+    self.zoomScale = 1.0;
+    
+    // make a new UIImageView for the new image
+    imageView = [[UIImageView alloc] initWithImage:image];
+    [self addSubview:imageView];
+    
+    self.contentSize = [image size];
+    [self setMaxMinZoomScalesForCurrentBounds];
+    self.zoomScale = self.minimumZoomScale;
+}
+
+- (void)displayTiledImageNamed:(NSString *)imageName size:(CGSize)imageSize
+{
+    // clear the previous imageView
+    [imageView removeFromSuperview];
+    [imageView release];
+    imageView = nil;
+    
+    // reset our zoomScale to 1.0 before doing any further calculations
+    self.zoomScale = 1.0;
+    
+    // make a new TilingView for the new image
+    imageView = [[TilingView alloc] initWithImageName:imageName size:imageSize];
+    [(TilingView *)imageView setAnnotates:YES]; // ** remove this line to remove the white tile grid **
+    [self addSubview:imageView];
+    
+    self.contentSize = imageSize;
+    [self setMaxMinZoomScalesForCurrentBounds];
+    self.zoomScale = self.minimumZoomScale;
+}
+
+- (void)setMaxMinZoomScalesForCurrentBounds
+{
+    CGSize boundsSize = self.bounds.size;
+    CGSize imageSize = imageView.bounds.size;
+    
+    // calculate min/max zoomscale
+    CGFloat xScale = boundsSize.width / imageSize.width;    // the scale needed to perfectly fit the image width-wise
+    CGFloat yScale = boundsSize.height / imageSize.height;  // the scale needed to perfectly fit the image height-wise
+    CGFloat minScale = MIN(xScale, yScale);                 // use minimum of these to allow the image to become fully visible
+    
+    // on high resolution screens we have double the pixel density, so we will be seeing every pixel if we limit the
+    // maximum zoom scale to 0.5.
+    CGFloat maxScale = 1.0 / [[UIScreen mainScreen] scale];
+    
+    // don't let minScale exceed maxScale. (If the image is smaller than the screen, we don't want to force it to be zoomed.) 
+    if (minScale > maxScale) {
+        minScale = maxScale;
+    }
+    
+    self.maximumZoomScale = maxScale;
+    self.minimumZoomScale = minScale;
+}
+
+#pragma mark -
+#pragma mark Methods called during rotation to preserve the zoomScale and the visible portion of the image
+
+// returns the center point, in image coordinate space, to try to restore after rotation. 
+- (CGPoint)pointToCenterAfterRotation
+{
+    CGPoint boundsCenter = CGPointMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds));
+    return [self convertPoint:boundsCenter toView:imageView];
+}
+
+// returns the zoom scale to attempt to restore after rotation. 
+- (CGFloat)scaleToRestoreAfterRotation
+{
+    CGFloat contentScale = self.zoomScale;
+    
+    // If we're at the minimum zoom scale, preserve that by returning 0, which will be converted to the minimum
+    // allowable scale when the scale is restored.
+    if (contentScale <= self.minimumZoomScale + FLT_EPSILON)
+        contentScale = 0;
+    
+    return contentScale;
+}
+
+- (CGPoint)maximumContentOffset
+{
+    CGSize contentSize = self.contentSize;
+    CGSize boundsSize = self.bounds.size;
+    return CGPointMake(contentSize.width - boundsSize.width, contentSize.height - boundsSize.height);
+}
+
+- (CGPoint)minimumContentOffset
+{
+    return CGPointZero;
+}
+
+// Adjusts content offset and scale to try to preserve the old zoomscale and center.
+- (void)restoreCenterPoint:(CGPoint)oldCenter scale:(CGFloat)oldScale
+{    
+    // Step 1: restore zoom scale, first making sure it is within the allowable range.
+    self.zoomScale = MIN(self.maximumZoomScale, MAX(self.minimumZoomScale, oldScale));
+    
+    
+    // Step 2: restore center point, first making sure it is within the allowable range.
+    
+    // 2a: convert our desired center point back to our own coordinate space
+    CGPoint boundsCenter = [self convertPoint:oldCenter fromView:imageView];
+    // 2b: calculate the content offset that would yield that center point
+    CGPoint offset = CGPointMake(boundsCenter.x - self.bounds.size.width / 2.0, 
+                                 boundsCenter.y - self.bounds.size.height / 2.0);
+    // 2c: restore offset, adjusted to be within the allowable range
+    CGPoint maxOffset = [self maximumContentOffset];
+    CGPoint minOffset = [self minimumContentOffset];
+    offset.x = MAX(minOffset.x, MIN(maxOffset.x, offset.x));
+    offset.y = MAX(minOffset.y, MIN(maxOffset.y, offset.y));
+    self.contentOffset = offset;
+}
+
+@end
---
+++ Classes/PhotoViewController.m
@@ -0,0 +1,258 @@
+#import "PhotoViewController.h"
+#import "ImageScrollView.h"
+
+@implementation PhotoViewController
+
+#pragma mark -
+#pragma mark View loading and unloading
+
+- (void)loadView 
+{    
+    // Step 1: make the outer paging scroll view
+    CGRect pagingScrollViewFrame = [self frameForPagingScrollView];
+    pagingScrollView = [[UIScrollView alloc] initWithFrame:pagingScrollViewFrame];
+    pagingScrollView.pagingEnabled = YES;
+    pagingScrollView.backgroundColor = [UIColor blackColor];
+    pagingScrollView.showsVerticalScrollIndicator = NO;
+    pagingScrollView.showsHorizontalScrollIndicator = NO;
+    pagingScrollView.contentSize = [self contentSizeForPagingScrollView];
+    pagingScrollView.delegate = self;
+    self.view = pagingScrollView;
+    
+    // Step 2: prepare to tile content
+    recycledPages = [[NSMutableSet alloc] init];
+    visiblePages  = [[NSMutableSet alloc] init];
+    [self tilePages];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    [pagingScrollView release];
+    pagingScrollView = nil;
+    [recycledPages release];
+    recycledPages = nil;
+    [visiblePages release];
+    visiblePages = nil;
+}
+
+- (void)dealloc
+{
+    [pagingScrollView release];
+    [super dealloc];
+}
+
+
+#pragma mark -
+#pragma mark Tiling and page configuration
+
+- (void)tilePages 
+{
+    // Calculate which pages are visible
+    CGRect visibleBounds = pagingScrollView.bounds;
+    int firstNeededPageIndex = floorf(CGRectGetMinX(visibleBounds) / CGRectGetWidth(visibleBounds));
+    int lastNeededPageIndex  = floorf((CGRectGetMaxX(visibleBounds)-1) / CGRectGetWidth(visibleBounds));
+    firstNeededPageIndex = MAX(firstNeededPageIndex, 0);
+    lastNeededPageIndex  = MIN(lastNeededPageIndex, [self imageCount] - 1);
+    
+    // Recycle no-longer-visible pages 
+    for (ImageScrollView *page in visiblePages) {
+        if (page.index < firstNeededPageIndex || page.index > lastNeededPageIndex) {
+            [recycledPages addObject:page];
+            [page removeFromSuperview];
+        }
+    }
+    [visiblePages minusSet:recycledPages];
+    
+    // add missing pages
+    for (int index = firstNeededPageIndex; index <= lastNeededPageIndex; index++) {
+        if (![self isDisplayingPageForIndex:index]) {
+            ImageScrollView *page = [self dequeueRecycledPage];
+            if (page == nil) {
+                page = [[[ImageScrollView alloc] init] autorelease];
+            }
+            [self configurePage:page forIndex:index];
+            [pagingScrollView addSubview:page];
+            [visiblePages addObject:page];
+        }
+    }    
+}
+
+- (ImageScrollView *)dequeueRecycledPage
+{
+    ImageScrollView *page = [recycledPages anyObject];
+    if (page) {
+        [[page retain] autorelease];
+        [recycledPages removeObject:page];
+    }
+    return page;
+}
+
+- (BOOL)isDisplayingPageForIndex:(NSUInteger)index
+{
+    BOOL foundPage = NO;
+    for (ImageScrollView *page in visiblePages) {
+        if (page.index == index) {
+            foundPage = YES;
+            break;
+        }
+    }
+    return foundPage;
+}
+
+- (void)configurePage:(ImageScrollView *)page forIndex:(NSUInteger)index
+{
+    page.index = index;
+    page.frame = [self frameForPageAtIndex:index];
+    
+    // Use tiled images
+    //[page displayTiledImageNamed:[self imageNameAtIndex:index] size:[self imageSizeAtIndex:index]];
+    
+    // To use full images instead of tiled images, replace the "displayTiledImageNamed:" call
+    // above by the following line:
+	[page displayImage:[self imageAtIndex:index]];
+}
+
+
+#pragma mark -
+#pragma mark ScrollView delegate methods
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    [self tilePages];
+}
+
+#pragma mark -
+#pragma mark View controller rotation methods
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation 
+{
+    return YES;
+}
+
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    // here, our pagingScrollView bounds have not yet been updated for the new interface orientation. So this is a good
+    // place to calculate the content offset that we will need in the new orientation
+    CGFloat offset = pagingScrollView.contentOffset.x;
+    CGFloat pageWidth = pagingScrollView.bounds.size.width;
+    
+    if (offset >= 0) {
+        firstVisiblePageIndexBeforeRotation = floorf(offset / pageWidth);
+        percentScrolledIntoFirstVisiblePage = (offset - (firstVisiblePageIndexBeforeRotation * pageWidth)) / pageWidth;
+    } else {
+        firstVisiblePageIndexBeforeRotation = 0;
+        percentScrolledIntoFirstVisiblePage = offset / pageWidth;
+    }    
+}
+
+- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    // recalculate contentSize based on current orientation
+    pagingScrollView.contentSize = [self contentSizeForPagingScrollView];
+    
+    // adjust frames and configuration of each visible page
+    for (ImageScrollView *page in visiblePages) {
+        CGPoint restorePoint = [page pointToCenterAfterRotation];
+        CGFloat restoreScale = [page scaleToRestoreAfterRotation];
+        page.frame = [self frameForPageAtIndex:page.index];
+        [page setMaxMinZoomScalesForCurrentBounds];
+        [page restoreCenterPoint:restorePoint scale:restoreScale];
+        
+    }
+    
+    // adjust contentOffset to preserve page location based on values collected prior to location
+    CGFloat pageWidth = pagingScrollView.bounds.size.width;
+    CGFloat newOffset = (firstVisiblePageIndexBeforeRotation * pageWidth) + (percentScrolledIntoFirstVisiblePage * pageWidth);
+    pagingScrollView.contentOffset = CGPointMake(newOffset, 0);
+}
+
+#pragma mark -
+#pragma mark  Frame calculations
+#define PADDING  10
+
+- (CGRect)frameForPagingScrollView {
+    CGRect frame = [[UIScreen mainScreen] bounds];
+    frame.origin.x -= PADDING;
+    frame.size.width += (2 * PADDING);
+    return frame;
+}
+
+- (CGRect)frameForPageAtIndex:(NSUInteger)index {
+    // We have to use our paging scroll view's bounds, not frame, to calculate the page placement. When the device is in
+    // landscape orientation, the frame will still be in portrait because the pagingScrollView is the root view controller's
+    // view, so its frame is in window coordinate space, which is never rotated. Its bounds, however, will be in landscape
+    // because it has a rotation transform applied.
+    CGRect bounds = pagingScrollView.bounds;
+    CGRect pageFrame = bounds;
+    pageFrame.size.width -= (2 * PADDING);
+    pageFrame.origin.x = (bounds.size.width * index) + PADDING;
+    return pageFrame;
+}
+
+- (CGSize)contentSizeForPagingScrollView {
+    // We have to use the paging scroll view's bounds to calculate the contentSize, for the same reason outlined above.
+    CGRect bounds = pagingScrollView.bounds;
+    return CGSizeMake(bounds.size.width * [self imageCount], bounds.size.height);
+}
+
+
+#pragma mark -
+#pragma mark Image wrangling
+
+- (NSArray *)imageData {
+    static NSArray *__imageData = nil; // only load the imageData array once
+    if (__imageData == nil) {
+        // read the filenames/sizes out of a plist in the app bundle
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"ImageData" ofType:@"plist"];
+        NSData *plistData = [NSData dataWithContentsOfFile:path];
+        NSString *error; NSPropertyListFormat format;
+        __imageData = [[NSPropertyListSerialization propertyListFromData:plistData
+                                                        mutabilityOption:NSPropertyListImmutable
+                                                                  format:&format
+                                                        errorDescription:&error]
+                       retain];
+        if (!__imageData) {
+            NSLog(@"Failed to read image names. Error: %@", error);
+            [error release];
+        }
+    }
+    return __imageData;
+}
+
+- (UIImage *)imageAtIndex:(NSUInteger)index {
+    // use "imageWithContentsOfFile:" instead of "imageNamed:" here to avoid caching our images
+    NSString *imageName = [self imageNameAtIndex:index];
+    NSString *path = [[NSBundle mainBundle] pathForResource:imageName ofType:@"jpg"];
+    return [UIImage imageWithContentsOfFile:path];    
+}
+
+- (NSString *)imageNameAtIndex:(NSUInteger)index {
+    NSString *name = nil;
+    if (index < [self imageCount]) {
+        NSDictionary *data = [[self imageData] objectAtIndex:index];
+        name = [data valueForKey:@"name"];
+    }
+    return name;
+}
+
+- (CGSize)imageSizeAtIndex:(NSUInteger)index {
+    CGSize size = CGSizeZero;
+    if (index < [self imageCount]) {
+        NSDictionary *data = [[self imageData] objectAtIndex:index];
+        size.width = [[data valueForKey:@"width"] floatValue];
+        size.height = [[data valueForKey:@"height"] floatValue];
+    }
+    return size;
+}
+
+- (NSUInteger)imageCount {
+    static NSUInteger __count = NSNotFound;  // only count the images once
+    if (__count == NSNotFound) {
+        __count = [[self imageData] count];
+    }
+    return __count;
+}
+
+
+@end
